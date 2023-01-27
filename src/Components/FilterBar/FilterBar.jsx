928fcc02af3011f8aa8bb0b62cac39f57f0326aa
Robin Zhao <titanen.1987@hotmail.com>
2023-01-27 22:34:45 +0100

    share button added

--- src/Components/FilterBar/FilterBar.jsx
+++ src/Components/FilterBar/FilterBar.jsx
@@ -4,12 +4,21 @@ import './FilterBar.css'
 import React from 'react'
 
 const FilterBar = (props) => {
+
+    const shareButtonClicked = () => [
+        navigator.clipboard.writeText(window.location.href)
+    ]
     return (
         <>
-            <section className='filterBar'> 
-                        <button className='filterButtons' onClick={() => props.onChange("Racing")}>Racing</button>
-                        <button className='filterButtons' onClick={() => props.onChange("Sandbox")}>Sandbox</button>
-                        <button className='filterButtons' onClick={() => props.onChange("All")}>All</button>           
+            <section className='buttonBar'> 
+                <div className='filterButtonWrapper'>
+                    <button className='Buttons' onClick={() => props.onChange("Racing")}>Racing</button>
+                    <button className='Buttons' onClick={() => props.onChange("Sandbox")}>Sandbox</button>
+                    <button className='Buttons' onClick={() => props.onChange("All")}>All</button>
+                </div>
+                <div className='shareWrapper'>
+                    <button onClick={() => shareButtonClicked()} className='Buttons'>Share</button>
+                </div>                
             </section>
         </>
     )
